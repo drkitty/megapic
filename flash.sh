d76ace53646701bc35f0a087c2ec2c9faee2911b
drkitty <bumblingkitty@gmail.com>
2015-06-15 12:10:58 -0700

    Change binary name

--- flash.sh
+++ flash.sh
@@ -2,4 +2,4 @@
 
 set -o errexit
 
-avrdude -p m2560 -c stk500v2 -P /dev/ttyACM0 -U flash:w:pic.bin
+avrdude -p m2560 -c stk500v2 -P /dev/ttyACM0 -U flash:w:get_dev_id.bin
--- make.sh
+++ make.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 CFLAGS="-std=c99 -g -Wall -Wextra -Werror=implicit-function-declaration"
 
-avr-gcc $CFLAGS -mmcu=avr6 -mmcu=atmega2560 -Os -DF_CPU=16000000L -o pic timer.c utils.c usart.c main.c
-avr-objcopy -O binary pic pic.bin
+avr-gcc $CFLAGS -mmcu=avr6 -mmcu=atmega2560 -Os -DF_CPU=16000000L \
+	-o get_dev_id timer.c utils.c usart.c main.c
+avr-objcopy -O binary get_dev_id get_dev_id.bin
